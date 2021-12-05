101f133bc581381318bea6ab4fafffebc668e6b4
Sebastian Gonzalez <ext_sagonzalezp@falabella.cl>
2021-12-04 21:09:31 -0300

    handlers

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/sagonzalezp/twitt/bd"
+	"github.com/sagonzalezp/twitt/handlers"
+)
+
+func main() {
+	if bd.CheckConnection() == 0 {
+		log.Fatal("Sin conexion a la BD")
+		return
+	}
+	handlers.Handlers()
 }
